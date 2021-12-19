1a296f800b926b1b5fd907838789849cf4e94584
Heiser <heiser9295@hotmail.com>
2021-12-19 16:55:35 +0800

    merge csv and output to clean loading_area

---
+++ raw_data/resale_hdb/merge_and_export.py
@@ -0,0 +1,19 @@
+# merge all csv files and export to loading_area directory
+
+import pandas as pd
+import glob
+import os
+
+directory = r"raw_data\resale_hdb"
+
+csv_files_in_directory = glob.glob(os.path.join(directory,"*.csv"))
+
+df_from_each_file = (pd.read_csv(f) for f in csv_files_in_directory)
+
+concatenated_df = (pd.concat(df_from_each_file)
+    .sort_values(by='month', ignore_index=True)
+)
+concatenated_df['remaining_lease'] = concatenated_df['remaining_lease'].fillna("").astype(str)
+
+concatenated_df.to_csv("loading_area/resale_hdb/resale_hdb.csv")
+concatenated_df.to_parquet("loading_area/resale_hdb/resale_hdb.parquet")
