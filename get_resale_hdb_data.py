6e9d0da19f3bca1dc541327e72e775d28101e660
Heiser <heiser9295@hotmail.com>
2022-12-08 19:28:24 +0800

    fix bug passing in negative limit

--- get_resale_hdb_data.py
+++ get_resale_hdb_data.py
@@ -2,43 +2,36 @@ import requests
 from pprint import pprint
 import pandas as pd
 
-folder_path = 'raw_data/resale_hdb'
-filename = 'resale-flat-prices-based-on-registration-date-from-jan-2017-onwards.csv'
+folder_path = "raw_data/resale_hdb"
+filename = "resale-flat-prices-based-on-registration-date-from-jan-2017-onwards.csv"
 resource_id = {
-    '2017 onwards': 'f1765b54-a209-4718-8d38-a39237f502b3',
-    'Jan 2015 - Dec 2016': '1b702208-44bf-4829-b620-4615ee19b57c',
-    'Mar 2012 - Dec 2014': '83b2fc37-ce8c-4df4-968b-370fd818138b',
-    '2000 - Feb 2012': '8c00bf08-9124-479e-aeca-7cc411d884c4',
-    '1990 - 1999': 'adbbddd3-30e2-445f-a123-29bee150a6fe'
+    "2017 onwards": "f1765b54-a209-4718-8d38-a39237f502b3",
+    "Jan 2015 - Dec 2016": "1b702208-44bf-4829-b620-4615ee19b57c",
+    "Mar 2012 - Dec 2014": "83b2fc37-ce8c-4df4-968b-370fd818138b",
+    "2000 - Feb 2012": "8c00bf08-9124-479e-aeca-7cc411d884c4",
+    "1990 - 1999": "adbbddd3-30e2-445f-a123-29bee150a6fe",
 }
-data_gov_url = 'https://data.gov.sg/api/action/datastore_search'
+data_gov_url = "https://data.gov.sg/api/action/datastore_search"
 params = {
-    'resource_id': resource_id['2017 onwards'],
-    'limit': 1,
-    'sort': 'month',
+    "resource_id": resource_id["2017 onwards"],
+    "limit": 1,
+    "sort": "month",
 }
 
-df_old_csv  = pd.read_csv(f'{folder_path}/{filename}')
+df_old_csv = pd.read_csv(f"{folder_path}/{filename}")
 
 nrow_csv = len(df_old_csv)
 
-response_current_total = requests.get(
-    url = data_gov_url,
-    params = params
-)
+response_current_total = requests.get(url=data_gov_url, params=params)
 
-current_total = response_current_total.json()['result']['total']
+current_total = response_current_total.json()["result"]["total"]
 
-params.update(limit=current_total - nrow_csv)
-response = requests.get(
-    url = data_gov_url,
-    params = params
-)
+params.update(limit=current_total)
+response = requests.get(url=data_gov_url, params=params)
 
-new_rows = response.json()['result']['records']
+df_out = pd.json_normalize(response.json(), record_path=["result", "records"])
 
-df = df_old_csv.append(new_rows)
-df.to_csv(f'{folder_path}/{filename}', index=False)
+df_out.to_csv(f"{folder_path}/{filename}", index=False)
 
 # df.to_parquet('data.parquet')
 # df.to_feather('data.feather')
