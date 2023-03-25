e4ad99b3880e480f649470650d06c09ac3b98f80
Heiser <heiser9295@hotmail.com>
2023-03-25 21:52:05 +0800

    fix bug, need to see bytesio to 0

--- upload_to_supabase.py
+++ upload_to_supabase.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from io import BytesIO
+import pyarrow.parquet as pq
 
 
 def get_supabase_configs():
@@ -31,14 +32,16 @@ print(res.content)
 priv_filename = (
     "raw_data/private_residential_transactions/batch_1_2023-03-23.parquet.zstd"
 )
-url = f"{configs['endpoint']}/storage/v1/object/sg-housing-db/batch_1_2023-03-23"
-import pyarrow.parquet as pq
+url = f"{configs['endpoint']}/storage/v1/object/sg-housing-db/batch_1_2023-03-23.parquet.zstd"
 
 table = pq.read_table(priv_filename)
 
 f = BytesIO()
-writer = pq.ParquetWriter(f, table.schema)
+# writer = pq.ParquetWriter(f, table.schema)
+# writer.write_table(table)
+# writer.close() # close writer before seeking to 0
 pq.write_table(table, f)
+f.seek(0)  # seek to 0 so requests can .read() from the start of buffer
 res = requests.post(
     url=url,
     headers={
