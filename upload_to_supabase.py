ccb271519aacdebfe3ea8824ca7643197b79ebde
Heiser <heiser9295@hotmail.com>
2023-03-31 18:16:15 +0800

    shift execution of module

--- upload_to_supabase.py
+++ upload_to_supabase.py
@@ -61,35 +61,41 @@ def list_files_from_supabase_bucket(
     return res
 
 
-configs = get_supabase_configs()
-bucket = "sg-housing-db"
+if __name__ == "__main__":
 
-file_info_response = list_files_from_supabase_bucket(
-    configs, bucket, prefix="", filename="resale.parquet.zstd"
-)
-print(len(file_info_response.json()))
+    configs = get_supabase_configs()
+    bucket = "sg-housing-db"
 
-file_info_non_response = list_files_from_supabase_bucket(
-    configs, bucket, prefix="", filename="non-existent-file"
-)
-print(len(file_info_non_response.json()))
+    file_info_response = list_files_from_supabase_bucket(
+        configs, bucket, prefix="", filename="resale.parquet.zstd"
+    )
+    print(len(file_info_response.json()))
 
-filepath = "raw_data/resale_hdb/resale-flat-prices-based-on-registration-date-from-jan-2017-onwards.parquet.zstd"
-file = open(filepath, "rb")
-filename = "resale.parquet.zstd"
-res = upload_file_to_supabase(configs, bucket, filename, file)
-print(res.content)
+    file_info_non_response = list_files_from_supabase_bucket(
+        configs, bucket, prefix="", filename="non-existent-file"
+    )
+    print(len(file_info_non_response.json()))
 
-priv_filename = (
-    "raw_data/private_residential_transactions/batch_1_2023-03-23.parquet.zstd"
-)
-table = pq.read_table(priv_filename)
-f = BytesIO()
-# writer = pq.ParquetWriter(f, table.schema)
-# writer.write_table(table)
-# writer.close() # close writer before seeking to 0
-pq.write_table(table, f)
-f.seek(0)  # seek to 0 so requests can .read() from the start of buffer
+    filepath = "raw_data/resale_hdb/resale-flat-prices-based-on-registration-date-from-jan-2017-onwards.parquet.zstd"
+    file = open(filepath, "rb")
+    filename = "resale.parquet.zstd"
+    res = upload_file_to_supabase(configs, bucket, filename, file)
+    print(res.content)
 
-res = upload_file_to_supabase(configs, bucket, "batch_1_2023-03-23.parquet.zstd", f)
-print(res.content)
+    priv_filename = (
+        "raw_data/private_residential_transactions/batch_1_2023-03-23.parquet.zstd"
+    )
+    table = pq.read_table(priv_filename)
+    f = BytesIO()
+    # writer = pq.ParquetWriter(f, table.schema)
+    # writer.write_table(table)
+    # writer.close() # close writer before seeking to 0
+    pq.write_table(table, f)
+    f.seek(0)  # seek to 0 so requests can .read() from the start of buffer
+
+    res = upload_file_to_supabase(configs, bucket, "batch_1_2023-03-23.parquet.zstd", f)
+    print(res.content)
+
+    res = download_file_from_supabase(configs, bucket, "resale.parquet.zstd")
+    table = pq.read_table(BytesIO(res.content))
+    print(table)
