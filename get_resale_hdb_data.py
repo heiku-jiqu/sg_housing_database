17f203aa45b8360e72262ca4ee831a29696d1ac0
Heiser <heiser9295@hotmail.com>
2023-04-01 23:58:14 +0800

    update dictionary to enum

--- get_resale_hdb_data.py
+++ get_resale_hdb_data.py
@@ -1,20 +1,22 @@
 import requests
 from pyarrow import Table
 import pyarrow.parquet as pq
+from enum import Enum
+
+
+class ResourceID(Enum):
+    JAN2017_ONWARDS = "f1765b54-a209-4718-8d38-a39237f502b3"
+    JAN2015_TO_DEC2017 = "1b702208-44bf-4829-b620-4615ee19b57c"
+    MAR2012_TO_DEC2014 = "83b2fc37-ce8c-4df4-968b-370fd818138b"
+    JAN2000_TO_FEB2012 = "8c00bf08-9124-479e-aeca-7cc411d884c4"
+    JAN1990_TO_JAN1999 = "adbbddd3-30e2-445f-a123-29bee150a6fe"
 
 
 def request_resale_hdb_data():
-    resource_id = {
-        "2017 onwards": "f1765b54-a209-4718-8d38-a39237f502b3",
-        "Jan 2015 - Dec 2016": "1b702208-44bf-4829-b620-4615ee19b57c",
-        "Mar 2012 - Dec 2014": "83b2fc37-ce8c-4df4-968b-370fd818138b",
-        "2000 - Feb 2012": "8c00bf08-9124-479e-aeca-7cc411d884c4",
-        "1990 - 1999": "adbbddd3-30e2-445f-a123-29bee150a6fe",
-    }
     data_gov_url = "https://data.gov.sg/api/action/datastore_search"
 
     params = {
-        "resource_id": resource_id["2017 onwards"],
+        "resource_id": ResourceID.JAN2017_ONWARDS.value,
         "limit": 1,
         "sort": "month",
     }
