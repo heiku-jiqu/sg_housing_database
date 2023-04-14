81516c617e2817736d772e00a963db008b53f4b0
Heiser <heiser9295@hotmail.com>
2023-04-14 17:44:25 +0800

    display parquet file contents using duckdb

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,32 @@
+<script lang="ts">
+	import { initDB } from '$lib/duckdb';
+	import { detach_before_dev, onMount } from 'svelte/internal';
+	import { DuckDBDataProtocol } from '@duckdb/duckdb-wasm';
+	async function load_db() {
+		const duckdb = await initDB();
+		const pq_data = await fetch('/api/blob_data');
+		const pq_data_blob = await pq_data.blob();
+		console.log(pq_data_blob);
+		const parquetFile = new File([pq_data_blob], 'pq_file');
+		await duckdb.registerFileHandle(
+			'resale.parquet',
+			parquetFile,
+			DuckDBDataProtocol.BROWSER_FILEREADER,
+			true
+		);
+
+		const c = await duckdb.connect();
+		const res = await c.query(`
+            SELECT * FROM 'resale.parquet' LIMIT 5;
+        `);
+		return res;
+	}
+	let result_promise = load_db();
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#await result_promise then table}
+	{table}
+{/await}
