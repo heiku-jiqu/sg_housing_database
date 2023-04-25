c44b8bfbce3c0cced3d5ec0b061b2f949c0bfcb8
Heiser <heiser9295@hotmail.com>
2023-04-25 20:58:39 +0800

    change to svg renderer

--- frontend/src/lib/echarts.ts
+++ frontend/src/lib/echarts.ts
@@ -1,7 +1,7 @@
 import * as echarts from 'echarts';
 
 const chart = function (node: HTMLElement, chartOpt: any) {
-	var myChart = echarts.init(node);
+	var myChart = echarts.init(node, null, { renderer: 'svg' });
 
 	myChart.setOption({ ...chartOpt, height: 400, width: 400 });
 };
