441b7cefa7067000cc3dbc87b5efd9d6ce8a39d7
Heiser <heiser9295@hotmail.com>
2023-04-28 15:07:39 +0800

    add global stylesheet

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../styles.css';
+</script>
+
+<h1>Housing Prices Trend</h1>
+
+<slot />
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -24,7 +24,4 @@
 	};
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
 <Echarts options={opt} />
