37d680bdd9318565f6542269dd770191f03e295c
Heiser <heiser9295@hotmail.com>
2023-05-01 18:37:50 +0800

    add label for facets

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -60,13 +60,34 @@
 					fy: (d) => Math.floor((Number(d.facet_key) - 1) / 3),
 					fx: (d) => (Number(d.facet_key) - 1) % 3
 				}
-			)
+			),
+			Plot.text(
+				[
+					...new Map(
+						data.median_cost_per_month_per_town
+							.select(['town', 'facet_key'])
+							.toArray()
+							.map((x) => [x['town'], x])
+					).values()
+				],
+				{
+					text: (d) => d.town,
+					fy: (d) => Math.floor((Number(d.facet_key) - 1) / 3),
+					fx: (d) => (Number(d.facet_key) - 1) % 3,
+					frameAnchor: 'top-left',
+					dx: 6,
+					dy: 6
+				}
+			),
+			Plot.frame()
 		],
 		marginLeft: 70,
 		style: {
 			width: '1200px',
 			backgroundColor: 'transparent'
 		},
-		grid: true
+		grid: true,
+		fx: { axis: null },
+		fy: { axis: null }
 	}}
 />
