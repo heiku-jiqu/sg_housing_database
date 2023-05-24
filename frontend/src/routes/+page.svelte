18ecb889b139d81f36689dd2082619100b82ce2d
Heiser <heiser9295@hotmail.com>
2023-05-24 19:45:35 +0800

    delete redundant imports

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
-	import Echarts from '$lib/components/Echarts.svelte';
-	import ObsPlot from '$lib/components/ObsPlot.svelte';
 	import AvgPrice from '$lib/components/plots/AvgPrice.svelte';
 	import AvgPriceEcharts from '$lib/components/plots/AvgPriceEcharts.svelte';
 	import TransactionVolume from '$lib/components/plots/TransactionVolume.svelte';
 	import MedianCostPerTown from '$lib/components/plots/MedianCostPerTown.svelte';
-	import * as Plot from '@observablehq/plot';
 	export let data;
 </script>
 
