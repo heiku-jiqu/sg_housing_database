be19ff47166de4a14486958d87bd0a451e2ae5d0
Heiser <heiser9295@hotmail.com>
2023-05-26 17:40:13 +0800

    add animation

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -11,12 +11,18 @@
 		margin-top: 0;
 		font-size: 2.5rem;
 	}
-	h1::before {
+	h1:before {
 		content: '';
 		position: absolute;
 		left: 1.7rem;
 		top: 3.7rem;
 		width: 3.2rem;
+		transition: width 200ms ease-in-out;
 		border-bottom: 4px solid blue;
+		z-index: -1;
+	}
+	h1:hover:before {
+		width: 22rem;
+		transition: width 200ms ease-in-out;
 	}
 </style>
