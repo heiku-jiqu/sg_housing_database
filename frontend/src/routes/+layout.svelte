54ebcba45783a7376a667297f095165ce6f0e586
Heiser <heiser9295@hotmail.com>
2023-06-02 23:29:35 +0800

    dont block h1

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,10 +1,39 @@
 <script>
+	import { initDB } from '$lib/duckdb';
 	import '../styles.css';
+	async function load_data() {
+		const duckdb = await initDB();
+
+		const [another_data, priv_resi_data] = await Promise.all([
+			fetch('/api/blob_data/hdb'),
+			fetch('/api/blob_data/private')
+		]);
+
+		const pq_data_array = new Uint8Array(await another_data.arrayBuffer());
+
+		const pq_priv_resi_array = new Uint8Array(await priv_resi_data.arrayBuffer());
+
+		await duckdb.registerFileBuffer('pq_file_buffer.parquet', pq_data_array);
+		await duckdb.registerFileBuffer('pq_priv_resi.parquet', pq_priv_resi_array);
+
+		const c = await duckdb.connect();
+		await c.query(`
+			CREATE TABLE IF NOT EXISTS resale_hdb AS
+			SELECT * FROM 'pq_file_buffer.parquet';
+		`);
+		await c.query(`
+			CREATE TABLE IF NOT EXISTS private_resi AS
+			SELECT * FROM 'pq_priv_resi.parquet';
+		`);
+	}
+	const promise = load_data();
 </script>
 
 <h1>Housing Prices Trend</h1>
 
-<slot />
+{#await promise then}
+	<slot />
+{/await}
 
 <style>
 	h1 {
---
+++ frontend/src/routes/using_store/+layout.ts
@@ -0,0 +1,2 @@
+// need render on client because WebWorker is clientside only
+export const ssr = false;
