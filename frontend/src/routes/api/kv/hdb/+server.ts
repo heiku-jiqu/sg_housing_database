114b74d19a41893d264778e639cc2a396ded11f6
Heiser <heiser9295@hotmail.com>
2023-08-17 21:49:29 +0800

    set some headers

--- frontend/src/routes/api/kv/hdb/+server.ts
+++ frontend/src/routes/api/kv/hdb/+server.ts
@@ -2,6 +2,13 @@ import type { RequestHandler } from '../$types';
 import { env } from '$env/dynamic/private';
 
 export async function GET({ url, platform }) {
-	let data = await platform?.env.KV.get('merged_hdb_resale_parquet_zstd');
-	return new Response(data);
+	let data = await platform?.env.KV.get('merged_hdb_resale_parquet_zstd', {
+		type: 'arrayBuffer',
+		cacheTtl: 86400
+	});
+	return new Response(data, {
+		headers: {
+			'Content-Type': 'application/octet-stream'
+		}
+	});
 }
