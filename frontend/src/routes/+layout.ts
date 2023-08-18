c644ce28538dd6fd6581e45fb0076240d932026d
Heiser <heiser9295@hotmail.com>
2023-08-18 13:49:31 +0800

    fix build

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -5,24 +5,27 @@ import {
 	avg_hdb_resale_store,
 	priv_residential_avg_cost
 } from '$lib/components/plots/store';
+import { browser } from '$app/environment';
 export const prerender = true;
 // need render on client because WebWorker is clientside only
 export const ssr = false;
 
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load({ data }) {
-	const tables_loaded = createTables();
-	// eagerly get query results
-	tables_loaded.then(() => {
-		transaction_vol_store.init();
-		median_cost_per_month_per_town.init();
-		avg_hdb_resale_store.init();
-		priv_residential_avg_cost.init();
-	});
-	await tables_loaded;
+	if (browser) {
+		const tables_loaded = createTables();
+		// eagerly get query results
+		tables_loaded.then(() => {
+			transaction_vol_store.init();
+			median_cost_per_month_per_town.init();
+			avg_hdb_resale_store.init();
+			priv_residential_avg_cost.init();
+		});
+		await tables_loaded;
 
-	const c = await connDB();
-	return {
-		dbconn: c
-	};
+		const c = await connDB();
+		return {
+			dbconn: c
+		};
+	}
 }
