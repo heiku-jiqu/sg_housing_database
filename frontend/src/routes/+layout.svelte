ee30621549bc286c43ac9cfd43d4ea3208441933
Heiser <heiser9295@hotmail.com>
2023-08-18 10:10:47 +0800

    use layout load function

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,29 +1,6 @@
 <script lang="ts">
-	import { initDB } from '$lib/duckdb';
 	import '../styles.css';
-	import {
-		transaction_vol_store,
-		median_cost_per_month_per_town,
-		avg_hdb_resale_store,
-		priv_residential_avg_cost
-	} from '$lib/components/plots/store';
 	import { goto } from '$app/navigation';
-	import type { AsyncDuckDBConnection } from '@duckdb/duckdb-wasm';
-
-	async function load_data() {
-		// let x = await c.query(`SELECT * FROM private_resi_unnest LIMIT 10;`);
-		// console.table(JSON.parse(JSON.stringify(x.toArray())));
-	}
-	const promise = load_data();
-
-	// eagerly get query results
-
-	// promise.then(() => {
-	// 	transaction_vol_store.init();
-	// 	median_cost_per_month_per_town.init();
-	// 	avg_hdb_resale_store.init();
-	// 	priv_residential_avg_cost.init();
-	// });
 </script>
 
 <h1 on:click={() => goto('/')}>Housing Prices Trend</h1>
@@ -34,6 +11,9 @@
 	h1 {
 		margin-top: 0;
 		font-size: 2.5rem;
+	}
+	h1:hover {
+		cursor: default;
 	}
 	h1:before {
 		content: '';
--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -1,3 +1,28 @@
+import { connDB, initDB, createTables } from '$lib/duckdb.js';
+import {
+	transaction_vol_store,
+	median_cost_per_month_per_town,
+	avg_hdb_resale_store,
+	priv_residential_avg_cost
+} from '$lib/components/plots/store';
 export const prerender = true;
 // need render on client because WebWorker is clientside only
 export const ssr = false;
+
+/** @type {import('./$types').LayoutServerLoad} */
+export async function load({ data }) {
+	const tables_loaded = createTables();
+	// eagerly get query results
+	tables_loaded.then(() => {
+		transaction_vol_store.init();
+		median_cost_per_month_per_town.init();
+		avg_hdb_resale_store.init();
+		priv_residential_avg_cost.init();
+	});
+	await tables_loaded;
+
+	const c = await connDB();
+	return {
+		dbconn: c
+	};
+}
