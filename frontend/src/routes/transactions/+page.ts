212ded92f82f77b5847884693c4d2eb241d05d26
Heiser <heiser9295@hotmail.com>
2023-09-01 14:11:21 +0800

    fix build?

--- frontend/src/routes/transactions/+page.ts
+++ frontend/src/routes/transactions/+page.ts
@@ -1,23 +1,27 @@
 import * as Arrow from 'apache-arrow';
 import { avg_hdb_resale_store } from '$lib/components/plots/store';
+import { browser } from '$app/environment';
+
 /** @type {import('./$types').PageLoad} */
 export async function load({ parent }) {
-	const { streamed } = await parent();
-	const c = await streamed?.dbconn;
-	await streamed?.tablesInitiated;
-	await avg_hdb_resale_store.init();
-	const prep_statement = await c?.prepare(`
+	if (browser) {
+		const { streamed } = await parent();
+		const c = await streamed?.dbconn;
+		await streamed?.tablesInitiated;
+		await avg_hdb_resale_store.init();
+		const prep_statement = await c?.prepare(`
 			SELECT * FROM resale_hdb
 			ORDER BY month DESC
 			LIMIT ?
 			;
 		`);
-	const result = await prep_statement.send(100000);
-	await result.open(); // need to open the reader to get schema!!!
-	const arrow_table = new Arrow.Table(result.schema);
+		const result = await prep_statement.send(100000);
+		await result.open(); // need to open the reader to get schema!!!
+		const arrow_table = new Arrow.Table(result.schema);
 
-	return {
-		arrow_table: arrow_table,
-		async_reader: result
-	};
+		return {
+			arrow_table: arrow_table,
+			async_reader: result
+		};
+	}
 }
