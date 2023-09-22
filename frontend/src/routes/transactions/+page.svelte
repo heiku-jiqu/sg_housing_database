b228807f1f2d4063f44060160a0000918f9e60f0
Heiser <heiser9295@hotmail.com>
2023-09-22 14:38:07 +0800

    refactor to not use load

--- frontend/src/routes/transactions/+page.svelte
+++ frontend/src/routes/transactions/+page.svelte
@@ -1,29 +1,19 @@
 <script lang="ts">
 	import { avg_hdb_resale_store } from '$lib/components/plots/store';
-	import * as Arrow from 'apache-arrow';
-	import { onMount } from 'svelte';
-	export let data;
+	import { tableStore as table, streamData } from './data';
 
-	$: arrow_table = data.arrow_table;
 	let num_rows_to_show = 10;
-	let arrow_slice: Arrow.Table;
-	$: if (arrow_table.numRows > 0) {
-		arrow_slice = arrow_table.slice(0, num_rows_to_show);
-	}
-	onMount(async () => {
-		for await (const batch of data.async_reader) {
-			arrow_table = arrow_table.concat(new Arrow.Table(batch));
-		}
-	});
+
+	streamData();
 </script>
 
 <a href="/">back to root</a>
-<div>{arrow_table.numRows} rows loaded</div>
+<div>{$table.numRows} rows loaded</div>
 <div>
 	<input
 		type="range"
 		min="0"
-		max={arrow_table.numRows}
+		max={$table.numRows}
 		bind:value={num_rows_to_show}
 		style:width="100%"
 	/>
@@ -31,17 +21,17 @@
 </div>
 <br />
 
-{#if arrow_table.numRows > 0}
+{#if $table.numRows > 0}
 	<table>
 		<tr>
-			{#each arrow_slice.schema.names as name}
+			{#each $table.schema.names as name}
 				<th>{name}</th>
 			{/each}
 		</tr>
-		{#each { length: arrow_slice.numRows } as _, i}
+		{#each { length: num_rows_to_show } as _, i}
 			<tr>
-				{#each arrow_slice.schema.names as name}
-					<td>{arrow_slice.get(i)[name]}</td>
+				{#each $table.schema.names as name}
+					<td>{$table.get(i)?.[name]}</td>
 				{/each}
 			</tr>
 		{/each}
---
+++ frontend/src/routes/transactions/data.ts
@@ -0,0 +1,22 @@
+import { avg_hdb_resale_store } from '$lib/components/plots/store';
+import { conn, tablesInitiated } from '$lib/duckdb2';
+import * as Arrow from 'apache-arrow';
+import { readable, writable } from 'svelte/store';
+
+await tablesInitiated;
+await avg_hdb_resale_store.init();
+const prep_statement = await conn.prepare(`
+			SELECT * FROM resale_hdb
+			ORDER BY month DESC
+			LIMIT ?
+			;
+		`);
+const result = await prep_statement.send(100000);
+await result.open(); // need to open the reader to get schema!!!
+export const tableStore = writable(new Arrow.Table(result.schema));
+export const streamData = async () => {
+	for await (const batch of result) {
+		tableStore.update((table) => table.concat(new Arrow.Table(batch)));
+	}
+};
+export { result as async_reader };
